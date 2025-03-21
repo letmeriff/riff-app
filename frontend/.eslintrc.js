057fbd922b48bd14a114851c7f0fa6b077bfa6b6
letmeriff <malte.risto@letmeriff.com>
2025-03-21 15:58:10 +0100

    fix: Simplify ESLint config and fix workflow

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,20 +1,33 @@
 module.exports = {
   extends: [
-    'react-app',
-    'react-app/jest',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
     'prettier'
   ],
   parser: '@typescript-eslint/parser',
-  rules: {
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'warn',
-    'react/prop-types': 'off'
+  plugins: ['@typescript-eslint', 'react'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
   },
   settings: {
     react: {
       version: 'detect'
     }
+  },
+  env: {
+    browser: true,
+    es2020: true,
+    jest: true
+  },
+  rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off'
   }
 }; 
