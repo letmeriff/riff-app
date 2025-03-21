acaadf4abf99021a52fb07f316e7605380770864
letmeriff <malte.risto@letmeriff.com>
2025-03-21 16:03:51 +0100

    fix: Simplify ESLint configuration to basics

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,22 +1,15 @@
 module.exports = {
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'prettier'
+    'plugin:@typescript-eslint/recommended'
   ],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'react'],
+  plugins: ['@typescript-eslint'],
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
     ecmaFeatures: {
       jsx: true
-    }
-  },
-  settings: {
-    react: {
-      version: 'detect'
     }
   },
   env: {
@@ -26,8 +19,6 @@ module.exports = {
   },
   rules: {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'warn',
-    'react/prop-types': 'off',
-    'react/react-in-jsx-scope': 'off'
+    '@typescript-eslint/no-explicit-any': 'warn'
   }
 }; 
