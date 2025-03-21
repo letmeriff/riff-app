0bf38eb7630459af6227bc016fdf7e051a79e2a8
letmeriff <malte.risto@letmeriff.com>
2025-03-21 16:06:04 +0100

    fix: Remove React plugin references from root ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,10 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'react'],
+  plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
     'prettier',
   ],
   env: {
@@ -14,15 +12,9 @@ module.exports = {
     node: true,
     es2021: true,
   },
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
   rules: {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'warn',
-    'react/prop-types': 'off',
   },
   ignorePatterns: ['dist/', 'build/', 'node_modules/', '*.config.js'],
 };
