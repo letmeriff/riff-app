5ecb5b2711805c337edd95043abfafe54adb72de
letmeriff <malte.risto@letmeriff.com>
2025-03-27 09:43:14 +0100

    Implement Phase 1 Week 1 Step 1: Configure Testing Frameworks

---
+++ frontend/jest.config.js
@@ -0,0 +1,28 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.test.json',
+      },
+    ],
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(yjs|y-protocols|y-websocket|y-indexeddb|lib0)/)',
+  ],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.stories.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/mocks/**',
+  ],
+};
